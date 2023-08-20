7f0d1e402b80ce711a912aafd67466692ba14c46
Himanshu <goyalhimanshu424@gmail.com>
2023-08-20 17:06:26 +0530

    getURLsFromHTML function

--- crawl.js
+++ crawl.js
@@ -5,13 +5,40 @@ Three steps in test wala development env.
 3) go back and implement the meat of function
 */
 
-/*  (normalization)
-    https://woodsleaf.com/ -> https://woodsleaf.com
-    http://woodsleaf.com -> https://woodsleaf.com
-    https://Woodsleaf.com -> https://woodsleaf.com
-*/
+
+const {JSDOM} = require('jsdom'); // for getURLsFromHTMl
+
+function getURLsFromHTML(htmlBody , baseURL){
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for(const linkElement of linkElements){
+        if(linkElement.href.slice(0,1) === '/'){
+            // relative url
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement}`);
+                urls.push(urlObj.href);
+            }catch(err){
+                console.log(`error with relative url: ${err.message}`);
+            }
+        }else{
+            try{
+                const urlObj = new URL(linkElement.href);
+                urls.push(urlObj.href);
+            }catch(err){
+                console.log(`error with absolute url: ${err.message}`);
+            }
+        }
+    }
+    return urls;
+}
 
 function normalizeURL(urlString){
+    /*  (normalization)
+        https://www.woodsleaf.com/ -> https://www.woodsleaf.com
+        http://www.woodsleaf.com -> https://www.woodsleaf.com
+        https://www.Woodsleaf.com -> https://www.woodsleaf.com
+    */
     const urlObj = new URL(urlString);
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
     if(hostPath.length > 0 && hostPath.slice(-1) === '/'){
@@ -21,5 +48,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,30 +1,91 @@
-const {normalizeURL} = require('./crawl.js');
+const {normalizeURL , getURLsFromHTML} = require('./crawl.js');
 const {test , expect} = require('@jest/globals');
 
+/********************** normalizeURL ****************************/
+
 test('normalizeURL strip protocol' , () => {
-    const input = "https://blog.boot.dev/path";
+    const input = "https://www.woodsleaf/path";
     const actual = normalizeURL(input);
-    const expected = "blog.boot.dev/path";
+    const expected = "www.woodsleaf/path";
     expect(actual).toEqual(expected);
 })
 
 test('normalizeURL strip trailing slash' , () => {
-    const input = "https://blog.boot.dev/path/";
+    const input = "https://www.woodsleaf/path/";
     const actual = normalizeURL(input);
-    const expected = "blog.boot.dev/path";
+    const expected = "www.woodsleaf/path";
     expect(actual).toEqual(expected);
 })
 
 test('normalizeURL capitals' , () => {
-    const input = "https://BLOG.boot.dev/path/";
+    const input = "https://www.WOODSLEAF/path/";
     const actual = normalizeURL(input);
-    const expected = "blog.boot.dev/path";
+    const expected = "www.woodsleaf/path";
     expect(actual).toEqual(expected);
 })
 
 test('normalizeURL http protocol' , () => {
-    const input = "http://Blog.boot.dev/path/";
+    const input = "http://www.woodsleaf/path";
     const actual = normalizeURL(input);
-    const expected = "blog.boot.dev/path";
+    const expected = "www.woodsleaf/path";
+    expect(actual).toEqual(expected);
+})
+
+/********************** getURLsFromHTML ****************************/
+
+test('getURLsFromHTML absolute' , () => {
+    const inputHTMLBody = `
+        <html>
+            <body>
+                <a href="https://www.woodsleaf.com/shop">Our Shop</a>
+            </body>
+        </html>
+    `;
+    const inputBaseURL = "https://www.woodsleaf.com";
+    const actual = getURLsFromHTML(inputHTMLBody , inputBaseURL);
+    const expected = ["https://www.woodsleaf.com/shop"];
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML relative' , () => {
+    const inputHTMLBody = `
+        <html>
+            <body>
+                <a href="/shop">Our Shop</a>
+            </body>
+        </html>
+    `;
+    const inputBaseURL = "https://www.woodsleaf.com";
+    const actual = getURLsFromHTML(inputHTMLBody , inputBaseURL);
+    const expected = ["https://www.woodsleaf.com/shop"];
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML both' , () => {
+    const inputHTMLBody = `
+        <html>
+            <body>
+                <a href="/shop">Our Shop</a>
+                <a href="https://www.woodsleaf.com/blogs">Our Blogs</a>
+            </body>
+        </html>
+    `;
+    const inputBaseURL = "https://www.woodsleaf.com";
+    const actual = getURLsFromHTML(inputHTMLBody , inputBaseURL);
+    const expected = ["https://www.woodsleaf.com/shop" , "https://www.woodsleaf.com/blogs"];
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML invalid' , () => {
+    const inputHTMLBody = `
+        <html>
+            <body>
+                <a href="invalid">Invalid url</a>
+            </body>
+        </html>
+    `;
+    const inputBaseURL = "https://www.woodsleaf.com";
+    const actual = getURLsFromHTML(inputHTMLBody , inputBaseURL);
+    const expected = [];
     expect(actual).toEqual(expected);
 })
